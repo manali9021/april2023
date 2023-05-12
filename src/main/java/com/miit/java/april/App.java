3636e4692f65323d58084c5af185cbf34bc52c53
Ishan <ishanpatel9234@gmail.com>
2023-05-11 22:26:10 -0300

    Initial

---
+++ src/main/java/com/miit/java/april/App.java
@@ -0,0 +1,13 @@
+package com.miit.java.april;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
